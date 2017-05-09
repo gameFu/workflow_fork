2fb92576e012b27755f3dd37091341766b34b958
gameFu <223328084@qq.com>
2017-05-09 11:18:54 +0800

    refactor test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,34 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'workflow_fork'
+require 'active_record'
+require 'sqlite3'
+require 'pry'
 
 require 'minitest/autorun'
+
+# 关闭migrate 输出
+ActiveRecord::Migration.verbose = false
+
+class << Minitest::Test
+  def test(name, &block)
+    test_name = :"test_#{name.gsub(' ','_')}"
+    raise ArgumentError, "#{test_name} is already defined" if self.instance_methods.include? test_name.to_s
+    if block
+      define_method test_name, &block
+    else
+      puts "PENDING: #{name}"
+    end
+  end
+end
+
+
+class ActiveRecordTestCase < Minitest::Test
+  def setup
+    ActiveRecord::Base.establish_connection(
+      :adapter => "sqlite3",
+      :database  => ":memory:" #"tmp/test"
+    )
+    # eliminate ActiveRecord warning. TODO: delete as soon as ActiveRecord is fixed
+    ActiveRecord::Base.connection.reconnect!
+  end
+end
--- test/workflow_column_test.rb
+++ test/workflow_column_test.rb
@@ -1,11 +1,4 @@
 require_relative 'test_helper'
-require 'active_record'
-require 'sqlite3'
-require 'workflow_fork'
-require 'pry'
-
-# 关闭migrate 输出
-ActiveRecord::Migration.verbose = false
 
 class Order < ActiveRecord::Base
   include WorkflowFork
@@ -16,16 +9,10 @@ class TestOrder < ActiveRecord::Base
   workflow_column :state
 end
 
-class TestWorkflow < Minitest::Test
+class TestWorkflowCloumn < ActiveRecordTestCase
 
   def setup
-    ActiveRecord::Base.establish_connection(
-      :adapter => "sqlite3",
-      :database  => ":memory:" #"tmp/test"
-    )
-
-    # eliminate ActiveRecord warning. TODO: delete as soon as ActiveRecord is fixed
-    ActiveRecord::Base.connection.reconnect!
+    super
     ActiveRecord::Schema.define do
       create_table :orders do |t|
         t.string :workflow_state
@@ -36,13 +23,13 @@ class TestWorkflow < Minitest::Test
     end
   end
 
-  def test_defalut_workflow_column
+  test 'defalut workflow column' do
     order = Order.new
     order.save
     assert_equal 'init', order.workflow_state
   end
 
-  def test_custome_workflow_column
+  test 'custome workflow column' do
     order = TestOrder.new
     order.save
     assert_equal 'init', order.state
